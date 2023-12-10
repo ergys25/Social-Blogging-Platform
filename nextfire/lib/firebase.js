1e701ec4d36e7e9544cab69d60b316147f68a8d1
Ergys Hila <ergysmania@gmail.com>
2023-12-10 17:42:41 -0500

    Add Firebase, React Firebase Hooks, and React Hot Toast dependencies Update _app.js to include Navbar and Toaster components Remove unnecessary imports and update Home component

---
+++ nextfire/lib/firebase.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/storage';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC2QqnV1JA3SBENzIycOs7KUOS4ENrRxZ8",
+    authDomain: "fir-d06ee.firebaseapp.com",
+    projectId: "fir-d06ee",
+    storageBucket: "fir-d06ee.appspot.com",
+    messagingSenderId: "1027974662903",
+    appId: "1:1027974662903:web:45533272f94ea1d1cd21f2",
+    measurementId: "G-QPWFLVMED4"
+  };
+
+    if (!firebase.apps.length) {
+        firebase.initializeApp(firebaseConfig);
+    }
+    export const auth = firebase.auth();
+    export const firestore = firebase.firestore();
+    export const storage = firebase.storage();
+
