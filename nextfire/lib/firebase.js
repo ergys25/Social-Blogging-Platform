7c42c6285dd077bfe39fed450b9eac1e9b792868
Ergys Hila <ergysmania@gmail.com>
2023-12-11 17:28:44 -0500

    Refactor code and add UserContext

--- nextfire/lib/firebase.js
+++ nextfire/lib/firebase.js
@@ -1,7 +1,9 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
-import 'firebase/storage';
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/storage";
+
+
 
 const firebaseConfig = {
     apiKey: "AIzaSyC2QqnV1JA3SBENzIycOs7KUOS4ENrRxZ8",
@@ -19,4 +21,5 @@ const firebaseConfig = {
     export const auth = firebase.auth();
     export const firestore = firebase.firestore();
     export const storage = firebase.storage();
+    export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
--- nextfire/pages/_app.js
+++ nextfire/pages/_app.js
@@ -1,16 +1,23 @@
 import '@/styles/globals.css'
 import Navbar from '@/components/Navbar'
 import { Toaster } from 'react-hot-toast'
+import { useUserData } from '@/lib/hooks'
+import { UserContext } from '@/lib/context';
 
+function App({ Component, pageProps }) {
+  const userData = useUserData();    
+    
+    
+    return (
+      <UserContext.Provider value={{ user: {}, username: 'Ergys' }}>
+        <Navbar />
+        <Component {...pageProps} />
+        <Toaster />
+      </UserContext.Provider>
+    );
+  }
 
-  function App({ Component, pageProps }) {
-  return (
-    <>
-      <Navbar />
-      <Component {...pageProps} />
-      <Toaster />
+     
 
-    </>
-  );
-}
+  
 export default App; 
